371a0ef2b95b5b9c8c2b02225ba41a24073f94e8
Vital9i <vitalikdoiniak@gmail.com>
2024-02-23 19:42:45 +0300

    HomeWork_4

--- index.js
+++ index.js
@@ -3,25 +3,21 @@ let timerId = null // переменная, которая будет храни
 
 const startButton = document.getElementById('start');
 startButton.addEventListener('click', function () {
-    // НУЖНО ПОГУГЛИТЬ ЧТО ТАКОЕ setInterval
-    timerId=setInterval(updateClock, 1000) // запускаем  updateClock() каждую секунду
+    timerId = setInterval(updateClock, 1000) // запускаем  updateClock() каждую секунду
 });
 
 
 const stopButton = document.getElementById('stop');
 stopButton.addEventListener('click', function () {
-    // НУЖНО ПОГУГЛИТЬ ЧТО ТАКОЕ clearInterval
-
-        clearInterval(timerId)// останавливаем таймер
+    clearInterval(timerId)// останавливаем таймер
 });
 
 
 function updateClock() {
     const clock = document.getElementById('clock');
-    // НУЖНО ПОГУГЛИТЬ ЧТО ТАКОЕ new Date()
     const now = new Date();
     const hours = now.getHours()
-    const minutes = now. getMinutes()
-    const seconds = now. getSeconds()
+    const minutes = now.getMinutes()
+    const seconds = now.getSeconds()
     clock.textContent = hours + ':' + minutes + ':' + seconds;
 }
